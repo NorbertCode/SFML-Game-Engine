c50f60c8bc7484cf2c41ed6d72499af476a1d9cc
Norbert <54719382+NorbertCode@users.noreply.github.com>
2019-10-25 16:28:16 +0200

    Update TextManager.h
    Added the option to change the text's and the outline's color

--- SFML Engine V2/src/TextManager.h
+++ SFML Engine V2/src/TextManager.h
@@ -8,7 +8,7 @@ namespace Engine
 		Assetmanager assets;
 		sf::Text text;
 	public:
-		void init(int charSize);
+		void init(int charSize, sf::Color textColor = sf::Color::White, sf::Color outlineColor = sf::Color::Black);
 		void update(std::string text, sf::Vector2f pos);
 		void draw(sf::RenderWindow &window);
 		sf::Text returnText();
