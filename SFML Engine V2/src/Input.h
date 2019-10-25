ea50b1d312a548d7f2d94a0427e62b6f30e0bac4
Norbert <54719382+NorbertCode@users.noreply.github.com>
2019-10-25 17:23:56 +0200

    Update Input.h

--- SFML Engine V2/src/Input.h
+++ SFML Engine V2/src/Input.h
@@ -6,8 +6,11 @@ namespace Engine
 	class Input
 	{
 	public:
-		//Checks if you clicked on a Sprite or a RectangleShape
+		//Checks if you clicked on a sprite or a rectangle
 		bool isButtonClicked(sf::RenderWindow &window, sf::Sprite button);
 		bool isButtonClicked(sf::RenderWindow &window, sf::RectangleShape button);
+		//Checks if mouse is hovering over a sprite or rectangle
+		bool isMouseOnButton(sf::RenderWindow &window, sf::Sprite button);
+		bool isMouseOnButton(sf::RenderWindow &window, sf::RectangleShape button);
 	};
 }
