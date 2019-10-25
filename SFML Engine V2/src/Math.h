d5b84d09c2d6fdaafb476141fc3ef727ac2d7ee6
Norbert <54719382+NorbertCode@users.noreply.github.com>
2019-10-25 17:22:43 +0200

    Update Math.h
    Added the option to centralize text

--- SFML Engine V2/src/Math.h
+++ SFML Engine V2/src/Math.h
@@ -8,8 +8,10 @@ namespace Engine
 		float randomize(float minNumber, float maxNumber); //Generates a random number
 		sf::Vector2f centralize(float maxSizeX, float maxSizeY, sf::Sprite object); //Returns the position between given values
 		sf::Vector2f centralize(float maxSizeX, float maxSizeY, sf::RectangleShape object);
+		sf::Vector2f centralize(float maxSizeX, float maxSizeY, sf::Text object);
 		sf::Vector2f centralize(sf::RenderWindow &window, sf::Sprite object); //Returns the position in the middle of the window 
 		sf::Vector2f centralize(sf::RenderWindow &window, sf::RectangleShape object);
+		sf::Vector2f centralize(sf::RenderWindow &window, sf::Text object);
 		//If you want to centralize in the left half of your screen you just make maxSizeX half of your screen width or height
 		//All methods of centralizing also remove half of the object's width and height
 	};
